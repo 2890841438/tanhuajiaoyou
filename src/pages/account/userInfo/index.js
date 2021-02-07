dd843328ed52ce9a4aeb548357c1a3d3122df459
dimples <2890841438@qq.com>
2021-02-07 17:03:11 +0800

    添加iconfont

--- src/pages/account/userInfo/index.js
+++ src/pages/account/userInfo/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { pxToDp } from '../../../utils/styleKits';
+import SvgUri from 'react-native-svg-uri';
+import { male, female } from '../../../res/fonts/iconSvg';
 
 class Index extends Component {
   render() {
@@ -10,6 +12,12 @@ class Index extends Component {
         <Text style={{ fontSize: pxToDp(20), color: "#666", fontWeight: "bold" }}>提升我的魅力</Text>
         <Text style={{ fontSize: pxToDp(20), color: "#666", fontWeight: "bold" }}>填写资料</Text>
         {/* 1.0 标题  结束 */}
+        {/* 2.0 性别 开始 */}
+        <View style={{marginTop:pxToDp(20)}}>
+          <SvgUri svgXmlData={male} width="30" height="30" />
+          <SvgUri svgXmlData={female} width="30" height="30" />
+        </View>
+        {/* 2.0 性别 结束 */}
       </View>
     );
   }
